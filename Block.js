26097db4fd0d78fc27971a076888d77ac72b8763
shreyashPramod <65612631+shreyashPramod@users.noreply.github.com>
2020-07-13 09:18:08 -0700

    Add files via upload

--- Block.js
+++ Block.js
@@ -1,8 +1,11 @@
 class Block {
   constructor(x, y, width, height) {
     var options = {
+      friction :0.0, 
         'restitution':0.8
+       
     }
+    this.visibility = 225; 
     this.body = Bodies.rectangle(x, y, width, height, options);
     this.width = width;
     this.height = height;
